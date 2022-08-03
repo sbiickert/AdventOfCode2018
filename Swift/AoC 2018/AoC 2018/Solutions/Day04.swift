3aaf12383defd6efcddfde1a70b38baf324bd669
Simon Biickert <simon@biickert.ca>
2022-08-03 15:20:44 -0600

    Day 04 parts 1 and 2 complete in Swift

--- Swift/AoC 2018/AoC 2018/Solutions/Day04.swift
+++ Swift/AoC 2018/AoC 2018/Solutions/Day04.swift
@@ -14,7 +14,10 @@ class Day04: AoCSolution {
 		let events = parseEvents(input)
 		
 		let result = solvePartOne(events)
-		print("Part One: the guard id * the minutes = \(result)")
+		print("Part One: the guard id * the minute = \(result)")
+		
+		let result2 = solvePartTwo(events)
+		print("Part Two: the guard id * the minute = \(result2)")
 	}
 	
 	func solvePartOne(_ events: [GuardEvent]) -> Int {
@@ -32,7 +35,7 @@ class Day04: AoCSolution {
 			}
 		}
 
-		print(sleepRecord as AnyObject)
+		//print(sleepRecord as AnyObject)
 
 		var maxSleep = 0
 		var sleepiestGuard = events[0].guardID!
@@ -45,7 +48,77 @@ class Day04: AoCSolution {
 		
 		print("The sleepiest guard is guard #\(sleepiestGuard) with \(maxSleep) minutes")
 		
-		return 0
+		let filtered = events.filter {
+			$0.type != .start && $0.guardID == sleepiestGuard
+		}
+		
+		var minuteTracker = Dictionary<Int, Int>()
+		for m in 0...59 { minuteTracker[m] = 0 }
+		for i in stride(from: 0, to: filtered.count, by: 2) {
+			for m in filtered[i].minute..<filtered[i+1].minute {
+				minuteTracker[m]! += 1
+			}
+		}
+		
+		//print(minuteTracker as AnyObject)
+		
+		var maxRepeats = 0
+		var sleepiestMinute = -1
+		for m in minuteTracker.keys {
+			if minuteTracker[m]! > maxRepeats {
+				maxRepeats = minuteTracker[m]!
+				sleepiestMinute = m
+			}
+		}
+
+		print("The sleepiest minute is minute \(sleepiestMinute) where #\(sleepiestGuard) was asleep \(maxRepeats) times")
+
+		return sleepiestGuard * sleepiestMinute
+	}
+	
+	func solvePartTwo(_ events: [GuardEvent]) -> Int {
+		let filtered = events.filter {
+			$0.type != .start
+		}
+		
+		var guardIDs = Set<Int>()
+		for event in filtered { guardIDs.insert(event.guardID!) }
+		
+		var guardPeakSleep = Dictionary<Int, (Int, Int)>()
+		for guardID in guardIDs {
+			let guardEvents = filtered.filter { $0.guardID == guardID }
+			var minuteTracker = Dictionary<Int, Int>()
+			for m in 0...59 { minuteTracker[m] = 0 }
+			for i in stride(from: 0, to: guardEvents.count, by: 2) {
+				for m in guardEvents[i].minute..<guardEvents[i+1].minute {
+					minuteTracker[m]! += 1
+				}
+			}
+			var maxRepeats = 0
+			var sleepiestMinute = -1
+			for m in minuteTracker.keys {
+				if minuteTracker[m]! > maxRepeats {
+					maxRepeats = minuteTracker[m]!
+					sleepiestMinute = m
+				}
+			}
+			guardPeakSleep[guardID] = (sleepiestMinute, maxRepeats)
+		}
+		
+		var sleepiestGuard = -1
+		var sleepiestMinute = -1
+		var maxRepeats = 0
+		for guardID in guardPeakSleep.keys {
+			if guardPeakSleep[guardID]!.1 > maxRepeats {
+				sleepiestGuard = guardID
+				sleepiestMinute = guardPeakSleep[guardID]!.0
+				maxRepeats = guardPeakSleep[guardID]!.1
+			}
+		}
+		
+		print("The sleepiest minute is minute \(sleepiestMinute) where #\(sleepiestGuard) was asleep \(maxRepeats) times")
+		
+		return sleepiestGuard * sleepiestMinute
 	}
 	
 	func parseEvents(_ input: [String]) -> [GuardEvent] {
@@ -106,6 +179,14 @@ struct GuardEvent {
 			time = Date()
 			type = .err
 		}
+	}
+	
+	var hour: Int {
+		return Calendar.current.component(.hour, from: time)
+	}
+	
+	var minute: Int {
+		return Calendar.current.component(.minute, from: time)
 	}
 }
 
