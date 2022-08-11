9fef52e16ea6c9e02dc10ccf1f83787378401839
Simon Biickert <simon@biickert.ca>
2022-08-11 13:43:51 -0600

    Day 05 part 2 complete

--- Swift/AoC 2018/AoC 2018/Solutions/Day05.swift
+++ Swift/AoC 2018/AoC 2018/Solutions/Day05.swift
@@ -14,10 +14,13 @@ class Day05: AoCSolution {
 		let input = AoCUtil.readInputFile(named: filename, removingEmptyLines: true)
 		let polymer = SuitPolymer(units: input[0].map( { String($0) } ))
 		
-		let result = solvePartOne(polymer)
-		print("Part One: the length of the resulting polymer is \(result.stringValue.count).")
+		let result1 = solvePartOne(polymer)
+		print("Part One: the length of the resulting polymer is \(result1.stringValue.count).")
 		
-		return AoCResult(part1: String(result.stringValue.count), part2: nil)
+		let result2 = solvePartTwo(polymer)
+		print("Part One: the length of the resulting polymer is \(result2.stringValue.count).")
+		
+		return AoCResult(part1: String(result1.stringValue.count), part2: String(result2.stringValue.count))
 	}
 	
 	private func solvePartOne(_ polymer: SuitPolymer) -> SuitPolymer {
@@ -25,6 +28,56 @@ class Day05: AoCSolution {
 		return reactRegex(polymer)
 	}
 	
+	private func solvePartTwo(_ polymer: SuitPolymer) -> SuitPolymer {
+		var bestUnit: String? = nil
+		var bestPolymer: SuitPolymer? = nil
+		let letters = "abcdefghijklmnopqrstuvwxyz".map( { String($0) } )
+
+		for letter in letters {
+			let newPolymer = reactRegex(polymer, collapseUnitRegardlessOfPolarity: letter)
+			if bestPolymer == nil || newPolymer.units.count < bestPolymer!.units.count {
+				bestPolymer = newPolymer
+				bestUnit = letter
+			}
+		}
+		print("The best unit to remove was \(bestUnit!).")
+		
+		return bestPolymer!
+	}
+	
+	func reactRegex(_ polymer: SuitPolymer, collapseUnitRegardlessOfPolarity: String? = nil) -> SuitPolymer {
+		let lc = "abcdefghijklmnopqrstuvwxyz".map( { String($0) } )
+		let uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".map( { String($0) } )
+		
+		var reList = [NSRegularExpression]()
+		for i in 0..<lc.count {
+			let rE = try! NSRegularExpression(pattern: lc[i]+uc[i])
+			reList.append(rE)
+			let Re = try! NSRegularExpression(pattern: uc[i]+lc[i])
+			reList.append(Re)
+		}
+
+		var pStr = NSMutableString(string: polymer.stringValue)
+		
+		if let unit = collapseUnitRegardlessOfPolarity {
+			pStr = NSMutableString(string: pStr.replacingOccurrences(of: unit.lowercased(), with: ""))
+			pStr = NSMutableString(string: pStr.replacingOccurrences(of: unit.uppercased(), with: ""))
+		}
+
+		var len = 1000000
+		while pStr.length != len {
+			len = pStr.length
+			for re in reList {
+				re.replaceMatches(in: pStr, range: NSMakeRange(0, pStr.length), withTemplate: "")
+			}
+			//print(pStr.length)
+		}
+		
+		let str = String(pStr)
+		let newPolymer = SuitPolymer(units: str.map( { String($0) } ))
+		return newPolymer
+	}
+
 	func reactWindowed(_ polymer: SuitPolymer) -> SuitPolymer {
 		var newPolymer = polymer
 		while newPolymer.units.count > 2 {
@@ -59,33 +112,6 @@ class Day05: AoCSolution {
 				break
 			}
 		}
-		return newPolymer
-	}
-	
-	func reactRegex(_ polymer: SuitPolymer) -> SuitPolymer {
-		let lc = "abcdefghijklmnopqrstuvwxyz".map( { String($0) } )
-		let uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".map( { String($0) } )
-		
-		var reList = [NSRegularExpression]()
-		for i in 0..<lc.count {
-			let rE = try! NSRegularExpression(pattern: lc[i]+uc[i])
-			reList.append(rE)
-			let Re = try! NSRegularExpression(pattern: uc[i]+lc[i])
-			reList.append(Re)
-		}
-		
-		let pStr = NSMutableString(string: polymer.stringValue)
-		var len = 1000000
-		while pStr.length != len {
-			len = pStr.length
-			for re in reList {
-				re.replaceMatches(in: pStr, range: NSMakeRange(0, pStr.length), withTemplate: "")
-			}
-			print(pStr.length)
-		}
-		
-		let str = String(pStr)
-		let newPolymer = SuitPolymer(units: str.map( { String($0) } ))
 		return newPolymer
 	}
 }
