8ab4e7e31a9b2c6a085f8825b1570cb66229e561
Simon Biickert <simon@biickert.ca>
2022-08-29 18:15:15 -0600

    Day 13 part 1 complete

--- Swift/AoC 2018/AoC 2018/Solutions/Day13.swift
+++ Swift/AoC 2018/AoC 2018/Solutions/Day13.swift
@@ -1,4 +1,6 @@
 import Foundation
+import Algorithms
+import SwiftUI
 
 class Day13: AoCSolution {
 	override init() {
@@ -11,21 +13,108 @@ class Day13: AoCSolution {
 		super.solve(filename: filename, index: index)
 		
 		let input = AoCUtil.readGroupedInputFile(named: filename, group: index)
-		//let parsed = parseMap(input)
 		
-		return AoCResult(part1: nil, part2: nil)
+		var parsed = parseMap(input)
+		let result1 = solvePartOne(map: parsed.map, carts: parsed.carts)
+		print("Part One: the location of the first crash is \(result1)")
+		
+		parsed = parseMap(input) // Re-parse. Part One will have changed the data
+		let result2 = solvePartTwo(map: parsed.map, carts: parsed.carts)
+		print("Part Two: the location of the last cart is \(result2)")
+
+		return AoCResult(part1: result1.description, part2: result2.description)
 	}
 	
-//	private func parseMap(_ input: [String]) -> (map: AoCGrid2D, carts: [MineCart]) {
-//		//var map = AoCGrid2D(
-//		
-//	}
+	private func solvePartOne(map: AoCGrid2D, carts: [MineCart]) -> AoCCoord2D {
+		var carts = carts
+		var collisionPosition: AoCCoord2D? = nil
+		
+		draw(map: map, carts: carts, mark: collisionPosition)
+
+		while collisionPosition == nil {
+			// Sort carts by position row, col
+			carts = carts.sorted {
+				$0.position.y == $1.position.y ?
+				$0.position.x < $1.position.x :
+				$0.position.y < $1.position.y
+			}
+			
+			for cart in carts {
+				let positions = Set<AoCCoord2D>(carts.map {$0.position})
+				cart.move(on: map.value(at: cart.position))
+				if positions.contains(cart.position) {
+					collisionPosition = cart.position
+					break
+				}
+			}
+			draw(map: map, carts: carts, mark: collisionPosition)
+		}
+		
+		return collisionPosition!
+	}
+	
+	private func solvePartTwo(map: AoCGrid2D, carts: [MineCart]) -> AoCCoord2D {
+		return AoCCoord2D(x: 0, y: 0)
+	}
+	
+	private func draw(map: AoCGrid2D, carts: [MineCart], mark: AoCCoord2D? = nil) {
+		let ext = map.extent
+		if ext.width > 50 { return }
+		var cartPositions = Dictionary<AoCCoord2D, MineCart>()
+		carts.forEach {cartPositions[$0.position] = $0}
+		
+		for row in ext.min.y...ext.max.y {
+			var values = [String]()
+			for col in ext.min.x...ext.max.x {
+				let pos = AoCCoord2D(x: col, y: row)
+				if mark != nil && mark! == pos {
+					values.append("X")
+				}
+				else if cartPositions.keys.contains(pos) {
+					values.append(cartPositions[pos]!.direction.rawValue)
+				}
+				else {
+					values.append(map.value(at: pos))
+				}
+			}
+			print(values.joined(separator: ""))
+		}
+		print()
+	}
+
+	private func parseMap(_ input: [String]) -> (map: AoCGrid2D, carts: [MineCart]) {
+		var map = AoCGrid2D(defaultValue: " ")
+		var carts = [MineCart]()
+		let TRACK_PARTS: Set = ["+", "|", "-", "/", "\\"]
+		
+		for (r, c) in product(0..<input.count, 0..<input[0].count) {
+			let char = String(input[r][c])
+			if TRACK_PARTS.contains(char) {
+				map.setValue(char, at: AoCCoord2D(x: c, y: r))
+			}
+			else {
+				// A mine cart
+				if let dir = AoCDirection(rawValue: char) {
+					let cart = MineCart(position: AoCCoord2D(x: c, y: r), direction: dir)
+					carts.append(cart)
+					switch dir {
+						case .up, .down:
+							map.setValue("|", at: AoCCoord2D(x: c, y: r))
+						case .left, .right:
+							map.setValue("-", at: AoCCoord2D(x: c, y: r))
+					}
+				}
+			}
+		}
+		
+		return (map: map, carts: carts)
+	}
 }
 
 class MineCart {
 	var position: AoCCoord2D
 	var direction: AoCDirection
-	private var lastTurn = TurnDir.none
+	private var lastTurnChosen = TurnDir.right // So that the first turn will be to the left
 	
 	init(position pos: AoCCoord2D, direction dir: AoCDirection) {
 		position = pos
@@ -61,13 +150,15 @@ class MineCart {
 	}
 	
 	func chooseDirection() {
-		switch lastTurn {
+		switch lastTurnChosen {
 		case .none:
 			turnRight()
+			lastTurnChosen = .right
 		case .left:
-			lastTurn = .none
+			lastTurnChosen = .none
 		case .right:
 			turnLeft()
+			lastTurnChosen = .left
 		}
 	}
 	
@@ -82,7 +173,6 @@ class MineCart {
 		case .left:
 			direction = .down
 		}
-		lastTurn = .left
 	}
 	
 	func turnRight() {
@@ -96,7 +186,6 @@ class MineCart {
 		case .left:
 			direction = .up
 		}
-		lastTurn = .right
 	}
 	
 	enum TurnDir {
