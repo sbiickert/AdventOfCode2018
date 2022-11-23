f71e6a5571b9a6fcd67ee99731dab8232a3a7952
Simon Biickert <simon@biickert.ca>
2022-11-23 08:56:33 -0700

    Day 1 Part 2 complete but too slow

--- shell/day01.sh
+++ shell/day01.sh
@@ -5,23 +5,50 @@ echo "Running $0"
 # The input file name is the first and only arg
 filename=$1
 
+f_spaced=temp.spc.txt
+f_dup=temp.dup.txt
+f_unique=temp.uni.txt
+
+# Clean up any temp files
+rm temp*
+
 # Pre-process the file in sed to break up +/- and the number
-temp01=temp.01.txt
-sed 's/\([\+\-]\)/\1 /' $filename > $temp01
+sed 's/\([\+\-]\)/\1 /' $filename > $f_spaced
 
 freq=0
+iter=0
 
-while read line || [ -n "$line" ]; do
-a=( ${(s. .)line} )
-if [[ $a[1] == "+" ]]
-then
-(( freq = $freq + $a[2] ))
-else
-(( freq = $freq - $a[2] ))
-fi
-#echo $freq
-done < $temp01
+# create zero-length files
+touch $f_dup
+touch $f_unique
 
-echo "Part 1: the result is $freq"
+# While the file $f_dup is zero length
+while [ ! -s $f_dup ]; do
+	echo $iter
+	while read line || [ -n "$line" ]; do
+		a=( ${(s. .)line} )
+		if [[ $a[1] == "+" ]]
+		then
+		(( freq = $freq + $a[2] ))
+		else
+		(( freq = $freq - $a[2] ))
+		fi
+		
+		# See if $freq is in $f_unique
+		sed -n "/^$freq$/p" $f_unique > $f_dup
+		if [ -s $f_dup ]
+		then
+			echo "Part 2: the result is $freq"
+			break
+		fi
+		echo $freq >> $f_unique
+	done < $f_spaced
+	
+	if [[ $iter == 0 ]] then
+		echo "Part 1: the result is $freq"
+	fi
+	
+	(( iter = $iter + 1 ))
+done
 
-rm $temp01
+#rm $f_spaced $f_dup $f_unique
